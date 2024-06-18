789d480378800e2d4211a048047db5f4fffd21e1
smoeury-songvate <vatgaming287@gmail.com>
2024-06-18 16:47:36 +0700

    somthing new

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './service/app.service';
 import { AppController } from './controllers/app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './database/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nest-crud',
+      password: 'smoeury-songvat',
+      database: 'nest-sample5577',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/service/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/database/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async create(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+}
